98d46aabec3f0e2600e87e4a2a1f9ef3b1554741
Andreas Granig <agranig@sipwise.com>
2018-05-28 10:36:44 +0200

    Add svg to png conversion, removing grid from svg

--- westation/assets/meteocons-icons/svg2png.sh
+++ westation/assets/meteocons-icons/svg2png.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-W=160
+H=160
 
 mkdir -p PNG
 for i in SVG/*.svg; do
-    qlmanage -t -s $W -o PNG/ $i
+    base=$(basename "${i%.*}")
+    nogrid="SVG/${base}-nogrid.svg"
+    xmlstarlet ed -N ns=http://www.w3.org/2000/svg \
+        -d "//ns:g[contains(@id,'Grid_1_')]" "$i" > "$nogrid"
+    rsvg-convert -h $H "$nogrid" > PNG/"$base".png
 done
